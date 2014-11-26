0de8069825720cc7cc1a159f68949750b86cd864
Sam Philip <seivadmas@gmail.com>
2014-11-26 09:50:07 -0300

    Validation spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should require a name' do
+    @question = Question.create
+    expect(@question).to_not be_valid
+  end
+
+  it 'should requite a valid rating' do
+    @question = Question.create(name: 'Name')
+    @question.rating = -1
+
+    expect(@question).to_not be_valid
+
+    @question.rating = 50
+    expect(@question).to be_valid
+  end
+
+  it 'should create a new question with valid fields' do
+    @question = Question.new
+    @question.name = 'Name'
+    @question.rating = 50
+    @question.save
+    expect(@question.persisted?).to eq true
+  end
 end
